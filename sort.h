bcebce8f436d225cf2b71fb1ba940bc8eaf34ed9
KaziZarif <91010672+KaziZarif@users.noreply.github.com>
2024-07-08 13:59:01 +1000

    add sort.c and it's header file

---
+++ sort.h
@@ -0,0 +1,9 @@
+#ifndef SORT_H
+#define SORT_H
+
+void merge_sort(int a[], int length);
+void bubble_sort(int a[], int length);
+void insertion_sort(int a[], int length);
+void selection_sort(int a[], int length);
+
+#endif // SORT_H
